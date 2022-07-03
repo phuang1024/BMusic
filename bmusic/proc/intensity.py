b453c62e0391f315269a854d943552d052f6cd01
Patrick Huang <huangpatrick16777216@gmail.com>
2022-07-03 12:56:45 -0700

    Intensity fade procedure.

--- bmusic/proc/intensity.py
+++ bmusic/proc/intensity.py
@@ -6,9 +6,11 @@ string's vibration, etc.
 __all__ = (
     "Intensity",
     "IntensityOnOff",
+    "IntensityFade",
 )
 
 import bpy
+import numpy as np
 
 from .procedure import Procedure
 
@@ -16,6 +18,8 @@ from .procedure import Procedure
 class Intensity(Procedure):
     """
     Base intensity class.
+
+    Max intensity is determined by note velocity.
 
     Parameters
     ----------
@@ -33,6 +37,10 @@ class Intensity(Procedure):
 class IntensityOnOff(Intensity):
     """
     Turns on when a note starts and off when it ends.
+
+    Keyframe types:
+    - JITTER: Resting (intensity = 0).
+    - BREAKDOWN: Playing.
 
     Parameters
     ----------
@@ -64,8 +72,9 @@ class IntensityOnOff(Intensity):
                 keys.append((note.start-duration, 0, "JITTER"))
 
             # Playing through note
-            keys.append((note.start, 1, "MOVING_HOLD"))
-            keys.append((note.end, 1, "MOVING_HOLD"))
+            intensity = np.interp(note.velocity, [0, 127], [0, 1])
+            keys.append((note.start, intensity, "BREAKDOWN"))
+            keys.append((note.end, intensity, "BREAKDOWN"))
 
             # Resting after note (sooner if next note close).
             frame = min(note.end+duration, (note.end+next)/2)
@@ -73,3 +82,78 @@ class IntensityOnOff(Intensity):
 
             for frame, value, type in keys:
                 self.animkey.animate(frame, handle=handle, type=type, on=value)
+
+
+class IntensityFade(Intensity):
+    """
+    Turns on when a note starts and fades away.
+
+    Keyframe types:
+    - JITTER: Resting (intensity = 0).
+    - BREAKDOWN: Fading.
+    - EXTREME: Max intensity.
+
+    Parameters
+    ----------
+
+    fade_fac: Exponential decay factor in factor/sec.
+        Default 0.6
+
+    key_interval: Interval in frames for decay section keyframes. Avoids keyframing
+        every frame.
+        Default: 10
+
+    off_thres: Threshold for intensity to be considered off.
+        Default: 0.01
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.fade_fac = kwargs.get("fade_fac", 0.3)
+        self.key_interval = kwargs.get("key_interval", 10)
+        self.off_thres = kwargs.get("off_thres", 0.01)
+
+    def animate(self):
+        fac = self.fade_fac ** (1.0/bpy.context.scene.render.fps)
+        interval_fac = fac ** self.key_interval
+
+        # Whether last note ended (intensity reached 0) with enough time to require
+        # another keyframe at 0 before next note.
+        last_ended = True
+
+        for i, note in enumerate(self.midi):
+            last = note.prev.end if note.prev else -1e9
+            next = note.next.start if note.next else 1e9
+            long_pause = next > note.end + 3   # Long between this end and next start
+            end_frame = min(note.end, next)
+
+            intensity = np.interp(note.velocity, [0, 127], [0, 1])
+
+            # Initial intensity
+            if last_ended:
+                self.animkey.animate(note.start-1, type="JITTER", on=0)
+            self.animkey.animate(note.start, type="EXTREME", on=intensity)
+
+            # Fading
+            frame = note.start
+            while True:
+                intensity *= interval_fac
+                next_frame = frame + self.key_interval
+
+                if next_frame > end_frame:
+                    final_int = intensity * fac ** (end_frame-frame)
+                    self.animkey.animate(end_frame, type="BREAKDOWN", on=final_int)
+                    if long_pause:
+                        # Need to set to 0
+                        self.animkey.animate(end_frame+1, type="JITTER", on=0)
+                    break
+
+                if intensity < self.off_thres:
+                    self.animkey.animate(next_frame, type="JITTER", on=0)
+                    break
+
+                self.animkey.animate(next_frame, type="BREAKDOWN", on=intensity)
+
+                frame = next_frame
+
+            last_ended = long_pause
