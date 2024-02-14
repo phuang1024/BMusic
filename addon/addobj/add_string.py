b2bf3080b80894dac7e14562271144bcf8090cf4
Patrick Huang <huangpatrick16777216@gmail.com>
2024-02-13 18:48:51 -0800

    Add string main.

--- addon/addobj/add_string.py
+++ addon/addobj/add_string.py
@@ -3,6 +3,7 @@ Add string operator.
 """
 
 import bpy
+from bpy.props import *
 
 
 class BMUSIC_OT_AddString(bpy.types.Operator):
@@ -10,6 +11,32 @@ class BMUSIC_OT_AddString(bpy.types.Operator):
     bl_label = "Add String"
     bl_options = {"REGISTER", "UNDO"}
 
+    obj_name: StringProperty(
+        name="Object Name",
+        default="String",
+    )
+
+    length: FloatProperty(
+        name="Length",
+        default=1.0,
+    )
+
     def execute(self, context):
-        self.report({"WARNING"}, "Not implemented yet")
+        add_string_main(self, context)
         return {"FINISHED"}
+
+
+def add_string_main(self, context):
+    name = self.obj_name
+
+    curve = bpy.data.curves.new(name=f"{name}.curve", type="CURVE")
+    curve.dimensions = "3D"
+    sp = curve.splines.new("NURBS")
+    sp.use_endpoint_u = True
+    sp.points.add(3)   # Total 4 points
+    sp.order_u = 5
+    for i in range(4):
+        sp.points[i].co = (i/4 * self.length, 0, 0, 1)
+    obj_curve = bpy.data.objects.new(f"{name}.curve", curve)
+    context.collection.objects.link(obj_curve)
+
